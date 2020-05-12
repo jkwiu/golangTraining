29a73d0b2d69061ab9d6fd03422fbcc5f648cae9
jk <sizzflyer@gmail.com>
2020-05-12 18:00:05 +0900

    slice started

--- 2020study/test.go
+++ 2020study/test.go
@@ -2,26 +2,9 @@ package main
 
 import "fmt"
 
-type Student struct {
-	name string
-	age int
-
-	grade string
-	class string
-}
-
-func (s *Student) PrintSundjunk()  {
-	fmt.Println(s.class, s.grade)
-}
-
-func (s *Student) InputSungjuk(class string, grade string) {
-	s.class = class
-	s.grade = grade
-}
-
 func main() {
-	var s Student = Student{name:"jk", age:23, class:"수학", grade:"A++"}
+	e := make([]int, 2, 5)
 
-	s.InputSungjuk("과학", "C")
-	s.PrintSundjunk()
+	fmt.Println(cap(e), len(e))
+
 }
