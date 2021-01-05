fe8c24de1770d429920512fbeb9be6e96dc1e630
sizzflyer <sizzflyer@gmail.com>
2021-01-05 13:15:49 +0900

    with context cancle in goroutines

--- usefulcode/eachgoroutine/main.go
+++ usefulcode/eachgoroutine/main.go
@@ -3,50 +3,53 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"runtime"
 	"sync"
 	"time"
 )
 
+// UnifedSCADA is Object of total turbines
 type UnifedSCADA struct {
-	TotalData map[string]WPPIS `json:"totaldata"`
+	TotalData map[string]WPPIS `json:"totaldata,omitempty"`
 	Mu        *sync.Mutex
 }
 
+// WPPIS is Object of location turbine
 type WPPIS struct {
 	URL        string          `json:"url"`
 	UpdateRate uint16          `json:"updaterate"`
 	Time       string          `json:"time"`
 	HTTPState  uint16          `json:"httpstate"`
-	Turbines   LocationTurbine `json:"turbines"`
-	StartTime  time.Time       `json:"start-time"`
-}
-type LocationTurbine struct {
-	ActivePower       string        `json:"activepower"`
-	ReactivePower     string        `json:"reactivepower"`
-	TotalActiveEnergy string        `json:"totalactiveenergy"`
-	WindSpeed         string        `json:"windspeed"`
-	WindDirection     string        `json:"winddirection"`
-	Temperature       string        `json:"temperature"`
-	Turbine           []EachTurbine `json:"turbine"`
+	Turbines   locationTurbine `json:"turbines,omitempty"`
 }
 
-type EachTurbine struct {
-	Name          string `json:"name"`
-	WindSpeed     string `json:"windspeed"`
-	RotorSpeed    string `json:"rotorspeed"`
-	ActivePower   string `json:"activepower"`
-	WindDirection string `json:"winddirection"`
-	AlarmMsg      string `json:"alarmmsg,omitempty"`
-	TurbineStatus string `json:"turbinestatus"`
+type locationTurbine struct {
+	ActivePower       string        `json:"activepower,omitempty"`
+	ReactivePower     string        `json:"reactivepower,omitempty"`
+	TotalActiveEnergy string        `json:"totalactiveenergy,omitempty"`
+	WindSpeed         string        `json:"windspeed,omitempty"`
+	WindDirection     string        `json:"winddirection,omitempty"`
+	Temperature       string        `json:"temperature,omitempty"`
+	Turbine           []eachTurbine `json:"turbine,omitempty"`
 }
 
-type WPPISXML struct {
+type eachTurbine struct {
+	Name          string `json:"name,omitempty"`
+	WindSpeed     string `json:"windspeed,omitempty"`
+	RotorSpeed    string `json:"rotorspeed,omitempty"`
+	ActivePower   string `json:"activepower,omitempty"`
+	WindDirection string `json:"winddirection,omitempty"`
+	AlarmMsg      string `json:"alarmmsg,omitempty,omitempty"`
+	TurbineStatus string `json:"turbinestatus,omitempty"`
+}
+
+type wppisXML struct {
 	XMLName     xml.Name `xml:"WPPIS"`
 	Time        string   `xml:"time,attr"`
 	GeneralData struct {
@@ -168,61 +171,102 @@ type WPPISXML struct {
 
 // Start returns json data from scada servers
 func (w *WPPIS) Start(us *UnifedSCADA, isRestart chan *WPPIS) {
-	start := time.Now()
-	ctx, c := w.ToJSON(w.ToXML(w.GetReq(us)))
-	// restart := make(chan *WPPIS)
+	// start := time.Now()
 	for {
+		ctx, cancel := context.WithCancel(context.Background())
+		c := w.toJSON(ctx, w.toXML(ctx, w.getReq(ctx, us)))
 		select {
 		case i := <-c:
+			// 빈 값이 오면 함수 종료
+			if i.URL == "" {
+				defer cancel()
+				log.Println("빈 값이 와서 종료합니다 ...", i)
+				isRestart <- w
+				return
+			}
 			us.Mu.Lock()
 			us.TotalData[i.URL] = i
 			us.Mu.Unlock()
-		case <-ctx.Done():
-			fmt.Println("재시작 메시지 확인")
-			fmt.Println("해당URL: ", w.URL)
-			fmt.Println(w)
-			isRestart <- w
-			return
 		}
-		fmt.Printf("URL: %s 업데이트 주기: %d 실행시간: %v Goroutine수: %d\n", w.URL, w.UpdateRate, time.Since(start), runtime.NumGoroutine())
+		// fmt.Printf("URL: %s 업데이트 주기: %d 실행시간: %v Goroutine수: %d\n", w.URL, w.UpdateRate, time.Since(start), runtime.NumGoroutine())
+		time.Sleep(time.Duration(w.UpdateRate) * time.Millisecond)
+	}
+}
+
+func (w *WPPIS) Restart(us *UnifedSCADA, isRestart chan *WPPIS) {
+	// start := time.Now()
+	log.Printf("%s의 재시작 요청을 시작됩니다.\n", w.URL)
+	for {
+		ctx, cancel := context.WithCancel(context.Background())
+		c := w.toJSON(ctx, w.toXML(ctx, w.getReq(ctx, us)))
+		select {
+		case i := <-c:
+			// 빈 값이 오면 함수 종료
+			if i.URL == "" {
+				defer cancel()
+				log.Println("빈 값이 와서 종료합니다 ...")
+				isRestart <- w
+				return
+			}
+			log.Println(i)
+		}
+		// fmt.Printf("URL: %s 업데이트 주기: %d 실행시간: %v Goroutine수: %d\n", w.URL, w.UpdateRate, time.Since(start), runtime.NumGoroutine())
 		time.Sleep(time.Duration(w.UpdateRate) * time.Millisecond)
 	}
 }
 
 // Response -> byte
-func (w *WPPIS) GetReq(us *UnifedSCADA) (context.Context, <-chan []byte) {
-	ctx, cancel := context.WithCancel(context.Background())
+func (w *WPPIS) getReq(ctx context.Context, us *UnifedSCADA) <-chan []byte {
 	byteChan := make(chan []byte)
 	go func() {
 		defer close(byteChan)
-		tr := &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		}
-		client := &http.Client{Transport: tr}
-		res, err := client.Get("https://" + w.URL + "/anonymous/RealtimeData.xml?type=DETAIL")
-		if err != nil {
-			defer cancel()
-			// defer w.Start(us)
-			log.Println(err, "\n다시 시작합니다 ...")
-			log.Println(w.HTTPState)
+		select {
+		case <-ctx.Done():
 			return
+		default:
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: true,
+				},
+			}
+			client := &http.Client{
+				Transport: tr,
+				Timeout:   5000 * time.Millisecond,
+			}
+			log.Printf("\n\n%s에 대한 리퀘스트 시작 바로 직전 ...\n", w.URL)
+			res, err := client.Get("https://" + w.URL + "/anonymous/RealtimeData.xml?type=DETAIL")
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				log.Println("Timeout error!!!")
+				us.Mu.Lock()
+				us.TotalData[w.URL] = WPPIS{
+					URL:        w.URL,
+					UpdateRate: w.UpdateRate,
+					HTTPState:  http.StatusRequestTimeout,
+				}
+				us.Mu.Unlock()
+				return
+			}
+			if err != nil {
+				log.Println("getReq에서의 에러", err)
+				return
+			}
+			log.Printf("%s의 함수는 진행 중 ...\n", w.URL)
+			defer res.Body.Close()
+			byteValue, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Println(err)
+			}
+			byteChan <- byteValue
 		}
-		defer res.Body.Close()
-		byteValue, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			log.Println(err)
-		}
-		byteChan <- byteValue
 	}()
-	return ctx, byteChan
+	log.Printf("%s는 캔슬 요청을 보냈어도 여기까진 실행됨 ...\n", w.URL)
+	return byteChan
 }
 
 // Byte -> XML
-func (w *WPPIS) ToXML(ctx context.Context, res <-chan []byte) (context.Context, <-chan WPPISXML) {
-	xmlData := WPPISXML{}
-	xmlChan := make(chan WPPISXML)
+func (w *WPPIS) toXML(ctx context.Context, res <-chan []byte) <-chan wppisXML {
+	xmlData := wppisXML{}
+	xmlChan := make(chan wppisXML)
 	go func() {
 		defer close(xmlChan)
 		for r := range res {
@@ -237,11 +281,11 @@ func (w *WPPIS) ToXML(ctx context.Context, res <-chan []byte) (context.Context, 
 			}
 		}
 	}()
-	return ctx, xmlChan
+	return xmlChan
 }
 
 // XML -> JSON
-func (w *WPPIS) ToJSON(ctx context.Context, xmlData <-chan WPPISXML) (context.Context, <-chan WPPIS) {
+func (w *WPPIS) toJSON(ctx context.Context, xmlData <-chan wppisXML) <-chan WPPIS {
 	jsonData := WPPIS{}
 	jsonChan := make(chan WPPIS)
 	go func() {
@@ -252,18 +296,18 @@ func (w *WPPIS) ToJSON(ctx context.Context, xmlData <-chan WPPISXML) (context.Co
 			jsonData.Time = x.Time
 			jsonData.URL = w.URL
 			jsonData.UpdateRate = w.UpdateRate
-			jsonData.Turbines = LocationTurbine{
+			jsonData.Turbines = locationTurbine{
 				ActivePower:       location.ActivePower.Text,
 				ReactivePower:     location.ReactivePower.Text,
 				TotalActiveEnergy: location.TotalActiveEnergy.Text,
 				WindSpeed:         location.WindSpeed.Text,
 				WindDirection:     location.WindDirection.Text,
 				Temperature:       location.Temperature.Text,
-				Turbine:           []EachTurbine{},
+				Turbine:           []eachTurbine{},
 			}
 
 			for _, turbines := range x.GeneralData.WindTurbineSummary.TurbineSummary {
-				jsonData.Turbines.Turbine = append(jsonData.Turbines.Turbine, EachTurbine{
+				jsonData.Turbines.Turbine = append(jsonData.Turbines.Turbine, eachTurbine{
 					Name:          turbines.Name,
 					WindSpeed:     turbines.WindSpeed.Text,
 					RotorSpeed:    turbines.RotorSpeed.Text,
@@ -280,7 +324,7 @@ func (w *WPPIS) ToJSON(ctx context.Context, xmlData <-chan WPPISXML) (context.Co
 			}
 		}
 	}()
-	return ctx, jsonChan
+	return jsonChan
 }
 
 var us *UnifedSCADA
@@ -307,17 +351,24 @@ func main() {
 		go s.Start(us, isRestart)
 	}
 
-	for {
-		for ir := range isRestart {
-			fmt.Println("재시작합니다~~~~~~~~~~~~~~~~~~~")
-			go ir.Start(us, isRestart)
+	go func() {
+		for {
+			for ir := range isRestart {
+				log.Printf("%s의 리퀘스트 재시작 요청 ...\n\n", ir.URL)
+				log.Println("goroutine 갯수: ", runtime.NumGoroutine())
+				go ir.Restart(us, isRestart)
+				time.Sleep(1000 * time.Millisecond)
+			}
 		}
+	}()
+
+	for {
 		us.Mu.Lock()
-		// str, err := json.Marshal(us.TotalData)
-		// if err != nil {
-		// 	log.Println(err)
-		// }
-		// fmt.Println(string(str))
+		str, err := json.Marshal(us.TotalData)
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println(string(str))
 		us.Mu.Unlock()
 		time.Sleep(1000 * time.Millisecond)
 	}
